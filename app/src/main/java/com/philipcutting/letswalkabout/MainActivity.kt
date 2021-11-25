2f381428df2056a975c1e1f3a855c9a942c99355
Philip <phil@nevie.com>
2021-11-26 00:02:26 +0200

    Initial commit

---
+++ app/src/main/java/com/philipcutting/letswalkabout/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.philipcutting.letswalkabout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
