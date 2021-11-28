f9f75d59d45565df38d36bcbbe6039a44fa13ae2
Philip <phil@nevie.com>
2021-11-28 20:24:27 +0200

    firebase setup
    Took 1 hour 17 minutes

--- app/src/main/java/com/philipcutting/letswalkabout/MainActivity.kt
+++ app/src/main/java/com/philipcutting/letswalkabout/MainActivity.kt
@@ -2,10 +2,44 @@ package com.philipcutting.letswalkabout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
+import com.mapbox.maps.MapView
+import com.philipcutting.letswalkabout.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mapView: MapView
+    lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        mapView = binding.mapView
+
+
     }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView?.onDestroy()
+    }
+
 }
